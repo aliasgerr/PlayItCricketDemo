5cf77e1d685a6141372596cc2ea25508eb5dd69e
aliasgerr <MacAir@aliasgerr.local>
2014-12-11 22:15:05 +0530

    updated offline
    updated offline

--- www/js/services.js
+++ www/js/services.js
@@ -3,7 +3,7 @@ angular.module('starter.services', [])
 /**
  * A simple example service that returns some data.
  */
-.factory('Tournaments', function() {
+.factory('Tournaments', function($http) {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
