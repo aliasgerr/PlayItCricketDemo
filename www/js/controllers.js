f7ca105065f666dbdbc927ee79c1a9ac47fb4a93
aliasgerr <MacAir@aliasgerr.local>
2014-12-11 22:13:18 +0530

    updated offline
    updated offline

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,23 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope, Tournaments) {
+	$scope.days = Tournaments.getDays();
 	$scope.tournaments = Tournaments.all();
+	$scope.slideIndex = 1;
 	
+    $scope.$watch('slideIndex', function (newIndex, oldIndex) {
+     	console.log("OLD:: "+oldIndex+ "NEW:: "+newIndex + "slideIndex"+$scope.slideIndex);
+    });
+
+    $scope.datefilter = function(index){
+		return function(item){ 
+			var currentDate = new Date($scope.days[index]);
+			var t_startDate = new Date(item.date_start);
+			var t_endDate =  new Date(item.date_end);
+			var tempDate = new Date(Math.min(t_endDate,Math.max(currentDate, t_startDate)))
+			return (currentDate.getTime()==tempDate.getTime());
+		}
+	};
 })
 
 .controller('FriendsCtrl', function($scope, Tournaments) {
@@ -13,4 +28,4 @@ angular.module('starter.controllers', [])
   $scope.tournament = Tournaments.get($stateParams.id);
 })
 
-.controller('AccountCtrl', function($scope) {});
+.controller('AccountCtrl', function($scope) {})
